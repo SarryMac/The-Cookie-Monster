8394e7f097a06ef0dd4fbf893495044807cca839
Udai Arneja <udaiarneja@Udais-MacBook-Pro.local>
2023-12-16 17:42:40 +0000

    v2.0: word matching for broader cookie removals

--- The Cookie Monster/content.js
+++ The Cookie Monster/content.js
@@ -3,39 +3,78 @@ document.addEventListener('DOMContentLoaded', function() {
     // Here you can later add code to interact with web pages  
 });
 
-function findCookieConsentDialog(node = document) {
-    const rejectButton=document.getElementById("consent_blackbar");
-    console.log(rejectButton);
-    if(rejectButton){
-        rejectButton.remove();
-    }    
+const wordsToLookOutFor = [
+    'consent', 'cookie', 'privacy'
+]
 
-    document.getElementById("teconsent").remove()
+function removeCookieConsentDialog() {
+    const idsOnPage = getAllIdsOnDOM();
+    console.log(idsOnPage);
+
+    idsOnPage.forEach(idString => {
+        if(idString === 'truste-consent-text'){
+            console.log('truste-consent-text')
+        }
+        const idStringLower = idString.toLowerCase();
+
+        for (var i = 0, n = wordsToLookOutFor.length; i < n; ++i) {
+            var currentReferenceWord = wordsToLookOutFor[i];
+
+            if(idStringLower.includes(currentReferenceWord)){
+                console.log(idStringLower, currentReferenceWord);
+                deleteElement(idString);
+            }
+        }
+    })
     
 }
-findCookieConsentDialog()
 
-function handleMutations(mutations, observer) {
-    for (let mutation of mutations) {
-        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-            mutation.addedNodes.forEach(node => {
-                if (node.nodeType === Node.ELEMENT_NODE) { // Make sure it's an element node
-                    const dialog = findCookieConsentDialog(node);
-                    if (dialog) {
-                        rejectCookies(dialog);
-                        observer.disconnect(); // Optional: disconnect the observer after finding the dialog
-                    }
-                }
-            });
-        }
+const varWillNeverChange = 1;
+var varWillChange = 3;
+varWillChange = 4;
+
+// returns a list of all ids on the 
+function getAllIdsOnDOM() {
+    var allElements = document.getElementsByTagName("*");
+    var allIds = [];
+    for (var i = 0, n = allElements.length; i < n; ++i) {
+    var el = allElements[i];
+    if (el.id) { allIds.push(el.id); }
     }
+    return allIds;
 }
 
-// Create a new MutationObserver instance
-const observer = new MutationObserver(handleMutations);
+function deleteElement(idStringToDelete){
+    const elementToDelete = document.getElementById(idStringToDelete);
+    if(elementToDelete){
+        elementToDelete.remove();
+    }
+}
+removeCookieConsentDialog()
 
-// Options for the observer (what changes to observe)
-const config = { childList: true, subtree: true };
 
-// Start observing the document body for DOM changes
-observer.observe(document.body, config);
+
+// function handleMutations(mutations, observer) {
+//     for (let mutation of mutations) {
+//         if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+//             mutation.addedNodes.forEach(node => {
+//                 if (node.nodeType === Node.ELEMENT_NODE) { // Make sure it's an element node
+//                     const dialog = findCookieConsentDialog(node);
+//                     if (dialog) {
+//                         rejectCookies(dialog);
+//                         observer.disconnect(); // Optional: disconnect the observer after finding the dialog
+//                     }
+//                 }
+//             });
+//         }
+//     }
+// }
+
+// // Create a new MutationObserver instance
+// const observer = new MutationObserver(handleMutations);
+
+// // Options for the observer (what changes to observe)
+// const config = { childList: true, subtree: true };
+
+// // Start observing the document body for DOM changes
+// observer.observe(document.body, config);
